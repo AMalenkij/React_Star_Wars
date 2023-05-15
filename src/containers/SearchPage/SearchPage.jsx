c95adf0ca8a58787f0e7f4dbfe3073d4b0060769
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2023-05-15 23:49:57 +0200

    group search implemented

--- src/containers/SearchPage/SearchPage.jsx
+++ src/containers/SearchPage/SearchPage.jsx
@@ -22,15 +22,39 @@ import { CATEGORY } from '../../constants/swApiProps'
 
 function SearchPage() {
   const [inputSearchValue, setInputSearchValue] = useState('')
-  const debounceValue = useDebounce(inputSearchValue, 500)
+  const [filteredCategory, setFilteredCategory] = useState([])
+  const [selectAll, setSelectAll] = useState(false)
 
+  const handleSelectAll = () => {
+    if (selectAll) {
+      setFilteredCategory([])
+      setSelectAll(false)
+    } else {
+      setFilteredCategory(CATEGORY)
+      setSelectAll(true)
+    }
+  }
+
+  const handleCategoryToggle = (value) => {
+    if (filteredCategory.includes(value)) {
+      setFilteredCategory(filteredCategory.filter((cat) => cat !== value))
+      setSelectAll(false)
+    } else {
+      setFilteredCategory([...filteredCategory, value])
+      if (filteredCategory.length + 1 === CATEGORY.length) {
+        setSelectAll(true)
+      }
+    }
+  }
+
+  const debounceValue = useDebounce(inputSearchValue, 500)
   const urls = useMemo(
     () =>
-      CATEGORY.map(
+      filteredCategory.map(
         (value) =>
           HTTPS + SWAPI_ROOT + value + SWAPI_PARAM_SEARCH + debounceValue
       ),
-    [debounceValue]
+    [debounceValue, filteredCategory]
   )
 
   const { isLoading, error, data } = useQuery(
@@ -79,13 +103,48 @@ function SearchPage() {
   )
   return (
     <>
-      <h1 className="header__text">Search</h1>
+      <h1 className={styles.header__text}>Search</h1>
       <h2>{isLoading}</h2>
-      <UiInput
-        value={inputSearchValue}
-        handleInputChange={handleInputChange}
-        placeholder="Input character's name"
-      />
+      <div className={styles.nav__container}>
+        <div className={styles.input__container}>
+          <UiInput
+            value={inputSearchValue}
+            handleInputChange={handleInputChange}
+            placeholder="Input character's name"
+          />
+          <div>Selected categories: {filteredCategory.join(', ')}</div>
+        </div>
+        <nav>
+          <nav>
+            <ul>
+              <li>
+                <label htmlFor="select-all-checkbox">
+                  <input
+                    type="checkbox"
+                    checked={selectAll}
+                    onChange={handleSelectAll}
+                  />
+                  Select All
+                </label>
+              </li>
+              {CATEGORY.map((value) => (
+                <li key={value}>
+                  <label htmlFor={value}>
+                    <input
+                      id={value}
+                      type="checkbox"
+                      checked={filteredCategory.includes(value)}
+                      onClick={() => handleCategoryToggle(value)}
+                    />
+                    {value}
+                  </label>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </nav>
+      </div>
+
       <ul className={styles.list__container}>{dataForAllCategory}</ul>
     </>
   )
