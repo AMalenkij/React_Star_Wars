26293891a35a2f8d43cc8370610f490d3a90cae2
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2023-05-21 22:22:25 +0200

    refactor Header
    add tailwind
    add headless ui
    add react-select
    
    implemented selection menu
    
    the theme change functionality was also moved from the home page
    
    added icons to main items

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,36 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
+import Select from 'react-select'
+import React, { useState, Fragment } from 'react'
 
 import styles from './Header.module.css'
 import Favorite from '../Favorite/Favorite'
+import Popover from '../UI/UiPopover/Popover'
 
-function Header() {
+import svgFilms from './img/menuIcons/films.svg'
+import svgCharacters from './img/menuIcons/characters.svg'
+import svgSpecies from './img/menuIcons/species.svg'
+import svgStarships from './img/menuIcons/starships.svg'
+import svgVehicles from './img/menuIcons/vehicles.svg'
+import svgPlanets from './img/menuIcons/planets.svg'
+
+const options = [
+  { value: 'people', label: 'People', icon: svgCharacters },
+  { value: 'planets', label: 'Planets', icon: svgPlanets },
+  { value: 'films', label: 'Films', icon: svgFilms },
+  { value: 'species', label: 'Species', icon: svgSpecies },
+  { value: 'starships', label: 'Starships', icon: svgStarships },
+  { value: 'vehicles', label: 'Vehicles', icon: svgVehicles },
+]
+
+export default function Header() {
+  const [selectedOption, setSelectedOption] = useState('Select a category')
+  const navigate = useNavigate()
+
+  const handleOptionChange = (selected) => {
+    setSelectedOption(selected.value)
+    navigate(`/${selected.value}/?page=1`)
+  }
+
   return (
     <div className={styles.container}>
       <svg className="logo" alt="Star Wars" />
@@ -11,37 +38,42 @@ function Header() {
         <li>
           <NavLink to="/">Home</NavLink>
         </li>
-        <li>
-          <NavLink to="/people/?page=1">People</NavLink>
-        </li>
-        <li>
-          <NavLink to="/planets/?page=1">Planets</NavLink>
-        </li>
-        <li>
-          <NavLink to="/films/?page=1">Films</NavLink>
-        </li>
-        <li>
-          <NavLink to="/species/?page=1">Species</NavLink>
-        </li>
-        <li>
-          <NavLink to="/starships/?page=1">Starships</NavLink>
-        </li>
-        <li>
-          <NavLink to="/vehicles/?page=1">Vehicles</NavLink>
+        <li className={styles.selectContainer}>
+          <Select
+            defaultValue={options.find(
+              (option) => option.value === selectedOption
+            )}
+            onChange={handleOptionChange}
+            options={options.map((option) => ({
+              ...option,
+              label: (
+                <div className={styles.iconLabel}>
+                  <img
+                    src={option.icon}
+                    alt={option.label}
+                    style={{ width: 36 }}
+                  />
+                  <span className={styles.iconText}>{option.label}</span>
+                </div>
+              ),
+            }))}
+            isOptionSelected={(option) => option.value === selectedOption}
+            getOptionLabel={(option) => option.label}
+            getOptionValue={(option) => option.value}
+            isSearchable={false}
+            components={{
+              IndicatorSeparator: () => null,
+            }}
+          />
         </li>
         <li>
           <NavLink to="/search">Search</NavLink>
         </li>
-        <li>
-          <NavLink to="/not-found">Not Found</NavLink>
-        </li>
-        <li>
-          <NavLink to="/fail">Fail</NavLink>
-        </li>
       </ul>
-      <Favorite />
+      <div className="ml-auto flex gap-6">
+        <Popover />
+        <Favorite />
+      </div>
     </div>
   )
 }
-
-export default Header
--- src/containers/HomePage/HomePage.jsx
+++ src/containers/HomePage/HomePage.jsx
@@ -1,12 +1,1 @@
-import ChooseSide from '../../components/HomePage/ChooseSide/ChooseSide'
-
-export function HomePage() {
-  return (
-    <>
-      <h1 className="header__text">Choose your side</h1>
-      <ChooseSide />
-    </>
-  )
-}
-
-export default HomePage
+export default function HomePage() {}
