9425acebfc633bdd789c8d096825d32fc06bd377
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2023-08-20 19:12:34 +0200

    install  @headlessui/tailwindcss plugin to target

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gold: 'var(--color-gold )',
+        gray: 'var(--color-gray-dark)',
+        knob_base: 'var(--color-knob-light-blue)',
+        knob_elevation: 'var(--color-knob-elevation)',
+      },
+      boxShadow: {
+        iner: 'inset 0 4px 4px 0px rgba(0, 0, 0, 0.25)',
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('@headlessui/tailwindcss')],
 }
