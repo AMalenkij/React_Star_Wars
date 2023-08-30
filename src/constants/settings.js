fd1a515f8a1d96cfddc82a0e41e2f4744849bde5
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2023-08-31 00:15:42 +0200

    create setting file
    different site setting will be stored there

---
+++ src/constants/settings.js
@@ -0,0 +1,19 @@
+// color to use for svg
+export const COLOR_GOLD = '#EEBF00'
+
+export const CATEGORIES_SELECTED = [
+  'people',
+  'species',
+  'films',
+  'starships',
+  'planets',
+  'vehicles',
+]
+
+// search settings
+
+export const MAX_ENTRIES = 7
+export const SEARCH_ENDPOINT = '/search/value'
+export const SEARCH_RESULT_COUNT = 4
+// local storage key names
+export const LOCAL_STORAGE_RECENT_QUESTS = 'recentQuests'
