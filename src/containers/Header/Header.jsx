80f2b070700ac305637b96315297d70fe549ea89
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2023-09-09 17:57:44 +0200

    Create Header.jsx
    working on code readability
    as well as code decomposition

---
+++ src/containers/Header/Header.jsx
@@ -0,0 +1,41 @@
+import Favorite from '../../components/Favorites/Favorites'
+import Logo from '../../components/Header/Logo/Logo'
+import PopUpBox from '../../components/Header/PopUpBox/PopUpBox'
+import Library from '../../components/Header/Library/Library'
+
+export default function Header() {
+  return (
+    <header
+      className="
+       shadow-drop-300
+       container 
+       flex
+       border-2
+       border-white
+       rounded-3xl
+       bg-header
+       px-8 py-4 mt-6
+       justify-between
+       items-center
+       mx-auto
+       "
+    >
+      <Logo />
+      <nav className="hidden md:block">
+        <ul className="flex gap-6 font-bold">
+          <li>Home</li>
+          <li>
+            <Library />
+          </li>
+          <li>Search</li>
+        </ul>
+      </nav>
+      <div className="md:hidden">
+        <PopUpBox />
+      </div>
+      <div className="hidden md:block">
+        <Favorite />
+      </div>
+    </header>
+  )
+}
