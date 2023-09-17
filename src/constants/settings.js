291bc3eec1d0a9c9ad9f440a52ba9c9f6f4c5d53
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2023-09-17 02:05:29 +0200

    added new setting for bg-circles

--- src/constants/settings.js
+++ src/constants/settings.js
@@ -24,3 +24,9 @@ export const CIRCLE_SETTINGS_FOR_POPUP = [
   { id: 'circle3', position: '-top-[26%] right-0', circleSize: 300 },
   { id: 'circle4', position: '-top-[14%] right-[4%]', circleSize: 220 },
 ]
+export const CIRCLE_SETTINGS_FOR_DETAIL_PAGE = [
+  { id: 'circle1', position: '-bottom-1/4 -left-[6%]', circleSize: 230 },
+  { id: 'circle2', position: 'top-1/3 left-1/4', circleSize: 90 },
+  { id: 'circle3', position: '-top-[26%] right-0', circleSize: 300 },
+  { id: 'circle4', position: '-top-[14%] right-[4%]', circleSize: 220 },
+]
